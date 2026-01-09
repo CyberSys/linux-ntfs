5d2fd14f3b9a771467eb01f4cc3329d78223e997
Hyunchul Lee <hyc.lee@gmail.com>
2026-01-09 11:15:43 +0900

    ntfs: fix iomap warings while running generic/269
    write_iomap_begin() should return an error code
    instead of false.
    
    WARNING: fs/iomap/iter.c:35 at iomap_iter+0x1e4/0x2ff, CPU#0: fsstress/2646
    ...
    Call Trace:
     <TASK>
     iomap_file_buffered_write+0xa1/0x33d
     ? lock_is_held_type+0x7f/0xda
     ntfs_file_write_iter+0x395/0x481 [ntfs]
     ? __pfx_ntfs_file_write_iter+0x10/0x10 [ntfs]
     vfs_write+0x11f/0x1a5
     ksys_write+0x87/0xce
     do_syscall_64+0xa1/0x28d
     entry_SYSCALL_64_after_hwframe+0x76/0x7e
    
    Signed-off-by: Hyunchul Lee <hyc.lee@gmail.com>

--- iomap.c
+++ iomap.c
@@ -509,7 +509,8 @@ remap_rl:
 	rl = __ntfs_attr_find_vcn_nolock(&ni->runlist, vcn);
 	if (IS_ERR(rl)) {
 		up_write(&ni->runlist.lock);
-		return false;
+		mutex_unlock(&ni->mrec_lock);
+		return -EIO;
 	}
 	lcn = ntfs_rl_vcn_to_lcn(rl, vcn);
 
